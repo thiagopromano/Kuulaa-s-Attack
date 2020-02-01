625ffa2bc5a9151ebc427efe96c80f415d49d64e
Thiago Romano <thiagopromano@gmail.com>
2020-02-01 13:28:37 -0300

    Full game working

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -7,10 +7,11 @@ public class GameDirector : MonoBehaviour
 {
 
     public PlayerTwoDirector playerTwoDirector;
-    
+    public GameObject hook;
     public int currentPlayer = 1;
     public static GameDirector director;
-
+    public GameObject kuulinhaPrefab;
+    
     void Start()
     {
         GameDirector.director = this;
@@ -24,5 +25,9 @@ public class GameDirector : MonoBehaviour
     public void EndSecondPlayerTurn()
     {
         Debug.Log("End player 2 turn");
+        var newKuulinhaPosition = hook.transform.position;
+        newKuulinhaPosition.z = newKuulinhaPosition.z - 1;
+        var newKuulinha = Instantiate(kuulinhaPrefab, newKuulinhaPosition, Quaternion.identity);
+        newKuulinha.GetComponent<SpringJoint2D>().connectedBody = hook.GetComponent<Rigidbody2D>();
     }
 }
