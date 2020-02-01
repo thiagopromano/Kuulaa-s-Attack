54fd37b8276e7432a1b503a4024998b95a2bfae8
Thiago Romano <thiagopromano@gmail.com>
2020-01-31 21:00:07 -0800

    First Commit

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    
+    private float releaseTime = .15f;
+
+    private bool isPressed = false;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        rb.isKinematic = true;
+        isPressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+}
