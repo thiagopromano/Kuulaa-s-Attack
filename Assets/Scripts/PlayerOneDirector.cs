3146247409e94f101c77f965e570da1b82e1245f
Thiago Romano <thiagopromano@gmail.com>
2020-02-01 19:18:16 -0300

    added missing files

---
+++ Assets/Scripts/PlayerOneDirector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneDirector : MonoBehaviour
+{
+    void Awake()
+    {
+    }
+
+    public void StartTurn()
+    {
+        this.enabled = true;
+    }
+}
